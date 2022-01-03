509971edd85b8949bb331c0ad13a18beb645b191
John Valincius <john@valincius.dev>
2022-01-03 13:41:40 -0500

    add hello world query

--- Sample/Server/Handlers/HelloWorldQueryHandler.cs
+++ Sample/Server/Handlers/HelloWorldQueryHandler.cs
@@ -5,6 +5,13 @@ namespace Sample.Server.Handlers
 {
     public class HelloWorldQueryHandler : IRequestHandler<HelloWorldQuery, string>
     {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public HelloWorldQueryHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
         public Task<string> Handle(HelloWorldQuery request, CancellationToken cancellationToken)
         {
             return Task.FromResult($"Hello, {request.Name} - Server time is {DateTime.UtcNow}");
